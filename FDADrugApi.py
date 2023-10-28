3760e7ea30218a297cec613fe590e18b651c0e6d
Kenneth-Tong <ktong2233@gmail.com>
2023-10-28 19:15:38 -0400

    FDA Drug API File Version 2: reformatted answers and now the responses are simpler and better formatted (warning labels and information about them)

--- FDADrugApi.py
+++ FDADrugApi.py
@@ -7,15 +7,26 @@ load_dotenv()
 FDA_API_KEY = os.getenv("FDA_API_KEY")  # Remember .env file!
 
 
-# TODO: set a final variable for the number of searches and interactions with other drugs
+# TODO: connect this to google maps for location (manufacture name)
 
-def get_drug_name(api_key, search, limit):
-    url = "https://api.fda.gov/drug/event.json?"
+# def get_drug_name(api_key, search, limit):
+#     url = "https://api.fda.gov/drug/event.json?"
+#     params = {
+#         "api_key": api_key,
+#         "search": search,
+#         "limit": limit,
+#         "count": "patient.drug.openfda.brand_name.exact"
+#     }
+#     response = requests.get(url=url, params=params)
+#     return response.json()
+
+
+def get_drug(api_key, search, limit):
+    url = "https://api.fda.gov/drug/label.json?"
     params = {
         "api_key": api_key,
         "search": search,
         "limit": limit,
-        "count": "patient.drug.openfda.brand_name.exact"
     }
     response = requests.get(url=url, params=params)
     return response.json()
@@ -33,23 +44,26 @@ def get_form(api_key, search, limit):
     return response.json()
 
 
-def get_ingredients(api_key, search, limit):
-    url = "https://api.fda.gov/drug/label.json?"
-    params = {
-        "api_key": api_key,
-        "search": "drug_interactions:" + search,
-        "limit": limit,
-        "count": "openfda.substance_name.exact"
-    }
-    response = requests.get(url=url, params=params)
-    return response.json()
+#
+#
+# def get_ingredients(api_key, search, limit):
+#     url = "https://api.fda.gov/drug/label.json?"
+#     params = {
+#         "api_key": api_key,
+#         "search": "drug_interactions:" + search,
+#         "limit": limit,
+#         "count": "active_ingredient"
+#     }
+#     response = requests.get(url=url, params=params)
+#     return response.json()
+#
+#
 
-
-def get_drug_reaction(api_key, search, limit):
+def get_patient_drug_reaction(api_key, search, limit):
     url = "https://api.fda.gov/drug/event.json?"
     params = {
         "api_key": api_key,
-        "search": "patient.drug.openfda.pharm_class_epc:" + search,
+        "search": search,
         "limit": limit,
         "count": "patient.reaction.reactionmeddrapt.exact"
     }
@@ -57,30 +71,58 @@ def get_drug_reaction(api_key, search, limit):
     return response.json()
 
 
-def convert_list(response, length, term):
+#
+#
+def convert_list(response, term, length):
     addList = list()
-    addList.append((response[0][term]).lower())
+    addList.append((response[0][term]).lower().capitalize())
     if length > 1:
         for i in (1, length - 1, 1):
-            addList.append((response[i][term]).lower())
+            addList.append((response[i][term]).lower().capitalize())
     addList = list(set(addList))  # conversion into a set then into a list for duplicates
     return addList
 
 
+class_name = "advil"
 search_number = 3
-form = get_form(FDA_API_KEY, "nonsteroidal+anti-inflammatory+drug", search_number)
-form_list = convert_list(form["results"], search_number, "term")
 
-reactions = get_drug_reaction(FDA_API_KEY, "nonsteroidal+anti-inflammatory+drug", 5)  # number of results
-reaction_list = convert_list(reactions["results"], 5, "term")
+drug = get_drug(FDA_API_KEY, class_name, 1)["results"][0]
 
-name = get_drug_name(FDA_API_KEY, "nonsteroidal+anti-inflammatory+drug", 1)
-name_list = convert_list(name["results"], 1, "term")
+# noinspection PyBroadException
+try:
+    do_not_use = ", ".join(drug["do_not_use"])
+except Exception as e:
+    do_not_use = ""
 
-ingredients = get_ingredients(FDA_API_KEY, "nonsteroidal+anti-inflammatory+drug", 1)
-ingredients_list = convert_list(ingredients["results"], 1, "term")
+# noinspection PyBroadException
+try:
+    warning = drug["warnings"]
+except Exception as e:
+    warning = drug["boxed_warning"]
 
-print("A good brand for this product: ", name_list)
-print("Forms of use: ", form_list)
-print("Side effects: ", reaction_list)
-print("Ingredients: ", ingredients_list)
+warning_string = ', '.join(warning)
+
+# noinspection PyBroadException
+try:
+    brand_name = " ".join(drug["openfda"]["brand_name"])
+except Exception as e:
+    brand_name = class_name
+
+form = get_form(FDA_API_KEY, class_name, search_number)
+form_list_string = convert_list(form["results"], "term", search_number)
+
+reactions = get_patient_drug_reaction(FDA_API_KEY, class_name, 5)["results"]  # number of results
+reaction_list = convert_list(reactions, "term", 5)
+
+print("Product name:", brand_name)
+
+# print("This is known as: ", name_list)
+print("Forms of use:", ', '.join(form_list_string))
+
+if do_not_use != "":
+    print("\n--[Important]--\n" + do_not_use)
+
+print("\n--[Warnings]--\n" + warning_string[9:len(warning_string)])
+
+print("\n--[Reported Side Effects]--\n" + ", ".join(reaction_list))
+# print("Ingredients: ", ingredients_list)
